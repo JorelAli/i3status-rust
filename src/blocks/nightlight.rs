575b7891556dac19e55273a9e86f5983b2dcae34
JorelAli <jorel.k.ali@gmail.com>
2019-03-15 09:52:37 +0000

    Adds nightlight module

--- src/blocks/nightlight.rs
+++ src/blocks/nightlight.rs
@@ -9,12 +9,12 @@ use config::Config;
 use de::deserialize_opt_duration;
 use errors::*;
 use widgets::button::ButtonWidget;
-use widget::{I3BarWidget, State};
+use widget::I3BarWidget;
 use input::I3BarEvent;
 
 use uuid::Uuid;
 
-pub struct NightLight {
+pub struct Toggle {
     text: ButtonWidget,
     command_on: String,
     command_off: String,
@@ -28,64 +28,48 @@ pub struct NightLight {
 
 #[derive(Deserialize, Debug, Default, Clone)]
 #[serde(deny_unknown_fields)]
-pub struct NightLightConfig {
+pub struct ToggleConfig {
     /// Update interval in seconds
     #[serde(default, deserialize_with = "deserialize_opt_duration")]
     pub interval: Option<Duration>,
 
     /// Shell Command to enable the toggle
-    #[serde(default = "NightLightConfig::default_command_on")]
     pub command_on: String,
 
     /// Shell Command to disable the toggle
-    #[serde(default = "NightLightConfig::default_command_off")]
     pub command_off: String,
 
     /// Shell Command to determine toggle state. <br/>Empty output => off. Any output => on.
-    #[serde(default = "NightLightConfig::default_command_state")]
     pub command_state: String,
 
     /// Icon ID when toggled on (default is "toggle_on")
-    #[serde(default = "NightLightConfig::default_icon_on")]
+    #[serde(default = "ToggleConfig::default_icon_on")]
     pub icon_on: String,
 
     /// Icon ID when toggled off (default is "toggle_off")
-    #[serde(default = "NightLightConfig::default_icon_off")]
+    #[serde(default = "ToggleConfig::default_icon_off")]
     pub icon_off: String,
 
     /// Text to display in i3bar for this block
     pub text: Option<String>,
 }
 
-impl NightLightConfig {
-
-    fn default_command_state() -> String {
-        "redshift -vx | grep Color".to_owned()
-    }
-
-    fn default_command_on() -> String {
-        "redshift -O 4500K".to_owned()
-    }
-
-    fn default_command_off() -> String {
-        "redshift -x".to_owned()
-    }
-
+impl ToggleConfig {
     fn default_icon_on() -> String {
-        "moon".to_owned()
+        "toggle_on".to_owned()
     }
 
     fn default_icon_off() -> String {
-        "sun".to_owned()
+        "toggle_off".to_owned()
     }
 }
 
-impl ConfigBlock for NightLight {
-    type Config = NightLightConfig;
+impl ConfigBlock for Toggle {
+    type Config = ToggleConfig;
 
     fn new(block_config: Self::Config, config: Config, _tx_update_request: Sender<Task>) -> Result<Self> {
         let id = Uuid::new_v4().simple().to_string();
-        Ok(NightLight {
+        Ok(Toggle {
             text: ButtonWidget::new(config, &id).with_content(block_config.text),
             command_on: block_config.command_on,
             command_off: block_config.command_off,
@@ -99,7 +83,7 @@ impl ConfigBlock for NightLight {
     }
 }
 
-impl Block for NightLight {
+impl Block for Toggle {
     fn update(&mut self) -> Result<Option<Duration>> {
         let output = Command::new(env::var("SHELL").unwrap_or("sh".to_owned()))
             .args(&["-c", &self.command_state])
@@ -131,12 +115,10 @@ impl Block for NightLight {
                 let cmd = if self.toggled {
                     self.toggled = false;
                     self.text.set_icon(self.icon_off.as_str());
-                    self.text.set_state(State::Idle);
                     &self.command_off
                 } else {
                     self.toggled = true;
                     self.text.set_icon(self.icon_on.as_str());
-                    self.text.set_state(State::Warning);
                     &self.command_on
                 };
 
