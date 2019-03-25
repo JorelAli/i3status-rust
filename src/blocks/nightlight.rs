0ad25bc8d646e16099d18b899f7441c15594cac1
JorelAli <jorel.k.ali@gmail.com>
2019-03-25 11:15:32 +0000

    Improves overall nightlight functionality - turns blue when "cool"

--- src/blocks/nightlight.rs
+++ src/blocks/nightlight.rs
@@ -18,7 +18,6 @@ pub struct NightLight {
     text: ButtonWidget,
     command_on: String,
     command_off: String,
-    command_state: String,
     icon_on: String,
     icon_off: String,
     update_interval: Option<Duration>,
@@ -42,10 +41,6 @@ pub struct NightLightConfig {
     #[serde(default = "NightLightConfig::default_command_off")]
     pub command_off: String,
 
-    /// Shell Command to determine toggle state. <br/>Empty output => off. Any output => on.
-    #[serde(default = "NightLightConfig::default_command_state")]
-    pub command_state: String,
-
     /// Icon ID when toggled on (default is "toggle_on")
     #[serde(default = "NightLightConfig::default_icon_on")]
     pub icon_on: String,
@@ -62,10 +57,6 @@ pub struct NightLightConfig {
 }
 
 impl NightLightConfig {
-
-    fn default_command_state() -> String {
-        "redshift -vx | grep Color".to_owned()
-    }
 
     fn default_command_on() -> String {
         "redshift -PO 4500K".to_owned()
@@ -97,7 +88,6 @@ impl ConfigBlock for NightLight {
             text: ButtonWidget::new(config, &id).with_content(block_config.text),
             command_on: block_config.command_on,
             command_off: block_config.command_off,
-            command_state: block_config.command_state,
             icon_on: block_config.icon_on,
             icon_off: block_config.icon_off,
             id,
@@ -113,10 +103,15 @@ impl Block for NightLight {
         
         if self.toggled {
             self.text.set_icon(self.icon_on.as_str());
+            if self.color_temperature > 8000 {
+                self.text.set_state(State::Info);
+            } else {
+                self.text.set_state(State::Warning);
+            }
         } else {
-             self.text.set_icon(self.icon_off.as_str());           
+            self.text.set_icon(self.icon_off.as_str());           
         }
-
+        
         Ok(self.update_interval)
     }
 
@@ -127,27 +122,43 @@ impl Block for NightLight {
     fn click(&mut self, e: &I3BarEvent) -> Result<()> {
         if let Some(ref name) = e.name {
             if name.as_str() == self.id {
-                self.text.set_text(self.color_temperature.to_string());
+                if self.toggled {
+                    self.text.set_text(self.color_temperature.to_string());
+                }
                 match e.button {
                     MouseButton::WheelUp => {
-                        self.color_temperature = self.color_temperature + 200;
-                        if self.color_temperature > 25000 {
-                            self.color_temperature = 25000;
+                        if self.toggled {
+                            self.color_temperature = self.color_temperature + 200;
+                            if self.color_temperature > 25000 {
+                                self.color_temperature = 25000;
+                            }
+                            Command::new(env::var("SHELL").unwrap_or("sh".to_owned()))
+                                .args(&["-c", &self.command_on.replace("4500", &self.color_temperature.to_string())])
+                                .output()
+                                .block_error("toggle", "failed to run toggle command")?;
+                            if self.color_temperature > 8000 {
+                                self.text.set_state(State::Info);
+                            } else {
+                                self.text.set_state(State::Warning);
+                            }
                         }
-                        Command::new(env::var("SHELL").unwrap_or("sh".to_owned()))
-                            .args(&["-c", &self.command_on.replace("4500", &self.color_temperature.to_string())])
-                            .output()
-                            .block_error("toggle", "failed to run toggle command")?;
                     }
                     MouseButton::WheelDown => {
-                        self.color_temperature = self.color_temperature - 200;
-                        if self.color_temperature < 1000 {
-                            self.color_temperature = 1000;
+                        if self.toggled {
+                            self.color_temperature = self.color_temperature - 200;
+                            if self.color_temperature < 1000 {
+                                self.color_temperature = 1000;
+                            }
+                            Command::new(env::var("SHELL").unwrap_or("sh".to_owned()))
+                                .args(&["-c", &self.command_on.replace("4500", &self.color_temperature.to_string())])
+                                .output()
+                                .block_error("toggle", "failed to run toggle command")?;
+                            if self.color_temperature > 8000 {
+                                self.text.set_state(State::Info);
+                            } else {
+                                self.text.set_state(State::Warning);
+                            }
                         }
-                        Command::new(env::var("SHELL").unwrap_or("sh".to_owned()))
-                            .args(&["-c", &self.command_on.replace("4500", &self.color_temperature.to_string())])
-                            .output()
-                            .block_error("toggle", "failed to run toggle command")?;
                     }
                     MouseButton::Left => {
                         let cmd = if self.toggled {
