e7fb30d826c75cda0338b574db8627de7cd62aea
JorelAli <jorel.k.ali@gmail.com>
2019-03-25 13:39:56 +0000

    Fix all bugs, split status working as intended now!

--- src/blocks/splitstatus.rs
+++ src/blocks/splitstatus.rs
@@ -24,7 +24,7 @@ pub struct SplitStatus {
     text: TextWidget,
     id: String,
     container_mapping: Arc<Mutex<HashMap<i64, bool>>>, //<Window ID, State (v = true, h = false)>
-    current_window: i64,
+    current_window: Arc<Mutex<i64>>,
 }
 
 #[derive(Deserialize, Debug, Default, Clone)]
@@ -42,7 +42,9 @@ impl ConfigBlock for SplitStatus {
 
         let container_mapping_original = Arc::new(Mutex::new(HashMap::new()));
         let container_mapping = container_mapping_original.clone();
-        let mut current_window = 0;
+
+        let current_window_original = Arc::new(Mutex::new(0i64));
+        let current_window = current_window_original.clone();
 
         thread::spawn(move || {
             // establish connection.
@@ -60,7 +62,8 @@ impl ConfigBlock for SplitStatus {
                         match the_command.as_ref() {
                             "split h" => {
                                     let mut container_mapping = container_mapping_original.lock().unwrap();
-                                    container_mapping.insert(current_window, false);
+                                    let mut current_window = current_window_original.lock().unwrap();
+                                    container_mapping.insert(*current_window, false);
 
                                     tx.send(Task {
                                         id: id_clone.clone(),
@@ -69,7 +72,8 @@ impl ConfigBlock for SplitStatus {
                             },
                             "split v" => {
                                     let mut container_mapping = container_mapping_original.lock().unwrap();
-                                    container_mapping.insert(current_window, true);
+                                    let mut current_window = current_window_original.lock().unwrap();
+                                    container_mapping.insert(*current_window, true);
 
                                     tx.send(Task {
                                         id: id_clone.clone(),
@@ -82,11 +86,11 @@ impl ConfigBlock for SplitStatus {
                     Event::WindowEvent(win_eventinfo) => {
                         let current_id = win_eventinfo.container.id;
                         let mut container_mapping = container_mapping_original.lock().unwrap();
-
+                        let mut current_window = current_window_original.lock().unwrap();
+                        *current_window = current_id;
                         match win_eventinfo.change {
                             WindowChange::New => {
                                 container_mapping.insert(current_id, false);
-                                current_window = current_id;
                                 tx.send(Task {
                                     id: id_clone.clone(),
                                     update_time: Instant::now(),
@@ -94,14 +98,12 @@ impl ConfigBlock for SplitStatus {
                             },
                             WindowChange::Close => {
                                 container_mapping.remove(&current_id);
-                                current_window = current_id;
                                 tx.send(Task {
                                     id: id_clone.clone(),
                                     update_time: Instant::now(),
                                 });
                             },
                             WindowChange::Focus => {
-                                current_window = current_id;
                                 tx.send(Task {
                                     id: id_clone.clone(),
                                     update_time: Instant::now(),
@@ -131,12 +133,15 @@ impl Block for SplitStatus {
         let container = self.container_mapping
             .lock()
             .block_error("Oops", "failed to aquire lock")?;
-        let my_state = container.get(&self.current_window);
+
+        let current_window2 = self.current_window
+            .lock()
+            .block_error("Oops", "failed to aquire lock")?;
+
+        let my_state = container.get(&current_window2);
+
         let current_state =  my_state.unwrap_or(&false);
-     /*   if *my_state {
-            self.text.set_icon("vertical");
-            self.text.set_state(State::Info);
-        }*/
+  
         if *current_state {
             self.text.set_icon("vertical");
             self.text.set_state(State::Info);
@@ -144,51 +149,11 @@ impl Block for SplitStatus {
             self.text.set_icon("horizontal");
             self.text.set_state(State::Good);
         }
-        
-        /*
-        let s = self.container_mapping.lock().unwrap();
-//        let current_state = (self.container_mapping
- //                            .lock()
-  //                           .block_error("oops", "Failed to lock")?)
-   //         .clone().get(&self.current_window);
-        let current_state = s.get(&self.current_window);
-        if *current_state.unwrap() {
-            self.text.set_icon("vertical");
-            self.text.set_state(State::Info);
-        } else {
-            self.text.set_icon("horizontal");
-            self.text.set_state(State::Good);
-        }*/
-
-        //let string = (*self.title
-        //    .lock()
-        //    .block_error("focused_window", "failed to acquire lock")?)
-        //    .clone();
-        //match string.as_ref() {
-        //    "splith" => {
-        //        self.text.set_icon("horizontal");
-        //        self.text.set_state(State::Good);
-        //    },
-        //    "splitv" => {
-        //        self.text.set_icon("vertical");
-        //        self.text.set_state(State::Info);
-        //    },
-        //    _ => {
-        //        self.text.set_icon("horizontal");
-        //        self.text.set_state(State::Good);
-        //    }
-//
-//        }
         Ok(None)
     }
 
     fn view(&self) -> Vec<&I3BarWidget> {
-        //let title = &*self.title.lock().unwrap();
-        //if String::is_empty(title) {
-        //    vec![]
-        //} else {
-            vec![&self.text]
-        //}
+        vec![&self.text]
     }
 
     fn id(&self) -> &str {
